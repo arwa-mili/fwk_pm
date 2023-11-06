d8dad8531cac0236cddd68db51ffe9770691e81b
Arwa <mili.arwa.ma@gmail.com>
2023-11-06 22:31:17 +0100

    flightsearch  implements service

---
+++ FlightSearchservice/src/main/java/flightService/service/Service/FlightSearchServiceImpl.java
@@ -0,0 +1,42 @@
+package flightService.service.Service;
+
+
+import flightService.service.Entity.Flight;
+import flightService.service.Entity.FlightSearchRequest;
+import flightService.service.Entity.FlightSearchResponse;
+import flightService.service.Repository.FlightSearchRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.BeanUtils;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class FlightSearchServiceImpl implements FlightSearchService {
+
+    private final FlightSearchRepository flightSearchRepository;
+
+    public List<FlightSearchResponse> searchFlights(FlightSearchRequest flightSearchRequest) {
+        List<Flight> flights = flightSearchRepository
+                .findByOriginAndDestinationAndDepartureDateGreaterThanEqualAndAvailableSeatsGreaterThanEqual(
+                        flightSearchRequest.origin(),
+                        flightSearchRequest.destination(),
+                        flightSearchRequest.travelDate(),
+                        flightSearchRequest.passengers());
+
+        List<FlightSearchResponse> flightSearchResponseList = flights
+                .stream()
+                .map(this::mapToFlightSearchResponse)
+                .collect(Collectors.toList());
+
+        return flightSearchResponseList;
+    }
+
+    private FlightSearchResponse mapToFlightSearchResponse(Flight flight) {
+        FlightSearchResponse flightSearchResponse = new FlightSearchResponse();
+        BeanUtils.copyProperties(flight, flightSearchResponse);
+        return flightSearchResponse;
+    }
+}
