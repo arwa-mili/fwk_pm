12a31d6d0ddfa0930896ca9ac555b6d83e7d1bfd
Arwa <mili.arwa.ma@gmail.com>
2023-11-06 22:23:41 +0100

    flightsearch req= res

---
+++ FlightSearchservice/src/main/java/flightService/service/Entity/FlightSearchRequest.java
@@ -0,0 +1,10 @@
+package flightService.service.Entity;
+
+import java.time.LocalDate;
+
+public record FlightSearchRequest(
+        String origin,
+        String destination,
+        LocalDate travelDate,
+        int passengers) {
+}
